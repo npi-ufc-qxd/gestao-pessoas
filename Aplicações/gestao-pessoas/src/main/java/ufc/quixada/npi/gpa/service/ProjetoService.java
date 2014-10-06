3865ec47f3afe26f39b655806b0f89f0618eb021
Cleiton Brito <cleitoncdsb@hotmail.com>
2014-10-06 10:31:03 -0300

    Arquivos de Arquiterura Base da Aplicação

---
+++ Aplicações/gestao-pessoas/src/main/java/ufc/quixada/npi/gpa/service/ProjetoService.java
@@ -0,0 +1,8 @@
+package ufc.quixada.npi.gpa.service;
+
+import ufc.quixada.npi.gpa.model.Projeto;
+
+public interface ProjetoService extends GenericService<Projeto> {
+
+
+}
---
+++ Aplicações/gestao-pessoas/src/main/java/ufc/quixada/npi/gpa/service/impl/ProjetoServiceImpl.java
@@ -0,0 +1,13 @@
+package ufc.quixada.npi.gpa.service.impl;
+
+import javax.inject.Named;
+
+import ufc.quixada.npi.gpa.model.Projeto;
+import ufc.quixada.npi.gpa.service.ProjetoService;
+
+@Named
+public class ProjetoServiceImpl extends GenericServiceImpl<Projeto> implements
+		ProjetoService {
+
+	
+}

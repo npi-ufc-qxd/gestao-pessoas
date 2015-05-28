6be862afea2d95570f19f8da26615e34e4e76c62
s-holmes <rayson-santos@gmail.com>
2015-05-28 13:00:59 -0300

    Atualização versão biblioteca NPI-CORE

--- aplicacoes/gestao-pessoas/src/main/java/ufc/quixada/npi/gp/service/impl/EstagiarioServiceImpl.java
+++ aplicacoes/gestao-pessoas/src/main/java/ufc/quixada/npi/gp/service/impl/EstagiarioServiceImpl.java
@@ -4,36 +4,35 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import ufc.quixada.npi.gp.model.Estagiario;
 import ufc.quixada.npi.gp.service.EstagiarioService;
 import br.ufc.quixada.npi.enumeration.QueryType;
-import br.ufc.quixada.npi.repository.GenericRepository;
 import br.ufc.quixada.npi.service.impl.GenericServiceImpl;
 
 @Named
 public class EstagiarioServiceImpl extends GenericServiceImpl<Estagiario> implements EstagiarioService{
 
-	
-	@Inject
-	private GenericRepository<Estagiario> estagiarioRepository;
-
 	@Override
 	public Estagiario getEstagiarioByPessoaId(Long id) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("id", id);
-		Estagiario estagiario = estagiarioRepository.findFirst(QueryType.JPQL, "select e from Estagiario e where e.pessoa.id = :id", params, 0);
+		
+		Estagiario estagiario = (Estagiario) findFirst(QueryType.JPQL, "select e from Estagiario e where e.pessoa.id = :id", params);
 		
 		return estagiario;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Estagiario> getEstagiarioTurma(Long id) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("id", id);
-		return estagiarioRepository.find(QueryType.JPQL, "select e from Estagiario e where e.turma.id = :id", params);
+		
+		List<Estagiario> estagiarios = find(QueryType.JPQL, "select e from Estagiario e where e.turma.id = :id", params);
+
+		return estagiarios;
 	}
 
 	@Override
@@ -41,7 +40,8 @@ public class EstagiarioServiceImpl extends GenericServiceImpl<Estagiario> implem
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("cpf", cpf);
 		params.put("senha", senha);
-		Estagiario estagiario = estagiarioRepository.findFirst(QueryType.JPQL, "select e from Estagiario e join e.pessoa p where p.cpf = :cpf and p.password = :senha", params, 0);
+
+		Estagiario estagiario = (Estagiario) findFirst(QueryType.JPQL, "select e from Estagiario e join e.pessoa p where p.cpf = :cpf and p.password = :senha", params);
 		
 		return estagiario;
 	}
---
+++ aplicacoes/gestao-pessoas/src/main/java/ufc/quixada/npi/gp/service/impl/FolgaServiceImpl.java
@@ -0,0 +1,11 @@
+package ufc.quixada.npi.gp.service.impl;
+
+import javax.inject.Named;
+
+import ufc.quixada.npi.gp.model.Folga;
+import ufc.quixada.npi.gp.service.FolgaService;
+import br.ufc.quixada.npi.service.impl.GenericServiceImpl;
+
+@Named
+public class FolgaServiceImpl extends GenericServiceImpl<Folga> implements FolgaService {
+}

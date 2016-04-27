f958938503bb480d2684b0f6f63a31880547ba7b
gabrielmendanha <gabrielmendanha@hotmail.com>
2016-04-27 10:15:49 -0300

    Submissão de avaliação de rendimento

--- aplicacoes/gestao-pessoas/src/main/java/ufc/quixada/npi/gp/controller/AvaliacaoController.java
+++ aplicacoes/gestao-pessoas/src/main/java/ufc/quixada/npi/gp/controller/AvaliacaoController.java
@@ -1,10 +1,13 @@
 package ufc.quixada.npi.gp.controller;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.apache.commons.fileupload.FileUpload;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -13,13 +16,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ufc.quixada.npi.gp.model.AvaliacaoRendimento;
+import ufc.quixada.npi.gp.model.Documento;
 import ufc.quixada.npi.gp.model.Estagiario;
 import ufc.quixada.npi.gp.model.Pessoa;
 import ufc.quixada.npi.gp.model.Submissao;
 import ufc.quixada.npi.gp.model.Turma;
+import ufc.quixada.npi.gp.model.enums.Tipo;
 import ufc.quixada.npi.gp.service.AvaliacaoService;
 import ufc.quixada.npi.gp.service.EstagiarioService;
 import ufc.quixada.npi.gp.service.PessoaService;
@@ -56,15 +63,31 @@ public class AvaliacaoController {
 	public String adicionarAvaliacaoEstagio(Model model,
 			@Valid @ModelAttribute("avaliacaoRendimento") AvaliacaoRendimento avaliacaoRendimento, HttpSession session,
 			RedirectAttributes redirect, @PathVariable("idEstagiario") Long idEstagiario,
-			@PathVariable("idTurma") Long idTurma) {
+			@PathVariable("idTurma") Long idTurma, @Valid @RequestParam("nota") Double nota, @Valid @RequestParam("rendimento") MultipartFile rendimento){
+
+		if(!rendimento.getContentType().equals("application/pdf")){
+			redirect.addFlashAttribute("error", "Escolha um arquivo pdf.");
+			return "redirect:/500";
+		}
+		
 		model.addAttribute("action", "cadastrar");
 		Pessoa pessoa = getUsuarioLogado(session);
 		Estagiario estagiario = estagiarioService.find(Estagiario.class, idEstagiario);
 		Turma turma = turmaService.getTurmaByIdAndEstagiarioId(idTurma, idEstagiario);
+		Tipo tipo = Tipo.AVALIACAO_RENDIMENTO;
+		
+		try {
+			turmaService.submeterDocumento(estagiario, turma, tipo, rendimento);
+			//turmaService.getSubmissaoByEstagiarioIdAndIdTurmaAndTipo(idEstagiario, idTurma, tipo);
+		} catch (IOException e) {
+			return "redirect:/500";
+		}
 		
 		avaliacaoRendimento.setSupervisor(pessoa);
 		avaliacaoRendimento.setTurma(turma);
 		avaliacaoRendimento.setEstagiario(estagiario);
+		avaliacaoRendimento.setNota(nota);
+		//avaliacaoRendimento.setDocumento(documento);
 		avaliacaoService.save(avaliacaoRendimento);
 		redirect.addFlashAttribute("success", "Avaliação cadastrada com sucesso.");
 

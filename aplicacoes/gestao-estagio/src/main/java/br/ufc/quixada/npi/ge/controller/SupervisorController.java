71ad9ee3861910cb0717cb6745fcc0838812f8b1
Rogerio Carvalho <rogerio.carvalho.silva15@gmail.com>
2016-07-04 11:29:21 -0300

    correção bug em adicionar turma sem orientador e supervisor

--- aplicacoes/gestao-estagio/src/main/java/br/ufc/quixada/npi/ge/controller/SupervisorController.java
+++ aplicacoes/gestao-estagio/src/main/java/br/ufc/quixada/npi/ge/controller/SupervisorController.java
@@ -118,16 +118,22 @@ public class SupervisorController {
 
 	@RequestMapping(value = "/Turma/Adicionar", method = RequestMethod.POST)
 	public String adicionarTurma(Model model, @Valid @ModelAttribute("turma") Turma turma,
-			@RequestParam("orientadorId") Long orientadorId, @RequestParam("supervisoresId") List<Long> supervisoresId,
+			@RequestParam(value = "orientadorId", required = false) Long orientadorId, @RequestParam(value = "supervisoresId", required = false) List<Long> supervisoresId,
 			RedirectAttributes redirect) {
 
-		Servidor orientador = pessoaService.buscarServidorPorId(orientadorId);
-
-		List<Servidor> supervisores = new ArrayList<Servidor>();
-		for (long supervisor : supervisoresId) {
-			supervisores.add(pessoaService.buscarServidorPorId(supervisor));
+		Servidor orientador = null;
+		if(orientadorId != null){
+			orientador = pessoaService.buscarServidorPorId(orientadorId);
 		}
 
+		List<Servidor> supervisores = null;
+		if(supervisoresId != null){
+			supervisores = new ArrayList<Servidor>();
+			for (long supervisor : supervisoresId) {
+				supervisores.add(pessoaService.buscarServidorPorId(supervisor));
+			}
+		}
+		
 		turma.setOrientador(orientador);
 		turma.setSupervisores(supervisores);
 
@@ -177,7 +183,10 @@ public class SupervisorController {
 
 		Turma turma = turmaService.buscarTurmaPorServidorId(idTurma,
 				pessoaService.buscarServidorPorCpf(getCpfUsuarioLogado()).getId());
-
+		if(turma == null){
+			redirect.addFlashAttribute("error", "Você não tem acesso");
+			return REDIRECT_PAGINA_INICIAL_SUPERVISOR;
+		}
 		model.addAttribute("turma", turma);
 
 		return DETALHES_TURMA;

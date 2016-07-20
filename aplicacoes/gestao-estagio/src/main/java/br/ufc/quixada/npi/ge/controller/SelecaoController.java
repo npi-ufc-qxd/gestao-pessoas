a986c0954bb7fe02ac37c72054ab1535a1ec802c
kerley <kerleysol@gmail.com>
2016-07-20 16:43:57 -0300

    Correções de erros

--- aplicacoes/gestao-estagio/src/main/java/br/ufc/quixada/npi/ge/controller/SelecaoController.java
+++ aplicacoes/gestao-estagio/src/main/java/br/ufc/quixada/npi/ge/controller/SelecaoController.java
@@ -10,7 +10,6 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.validation.Valid;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
